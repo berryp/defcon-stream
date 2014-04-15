2e2f09a901df4da56409570070abd8619f57e64a
Berry Phillips <berryphillips@gmail.com>
2014-04-15 17:05:40 -0400

    Add config object.

--- defcon.go
+++ defcon.go
@@ -7,9 +7,45 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
-const staticRoot = "src/defcon/app"
+const defaultStaticRoot = "src/defcon/app"
+const defaultHttpPort = 8080
+const defaultZeroMqUrl = "http://localhost:8888"
+
+type DefconConfig struct {
+	StaticRoot string
+	HttpPort int
+	ZeroMqUrl string
+}
+
+func NewConfig() DefconConfig {
+	var staticRoot = os.Getenv("DEFCON_STATIC_ROOT")
+	var httpPortStr = os.Getenv("DEFCON_HTTP_PORT")
+	var zeroMqUrl = os.Getenv("DEFCON_ZEROMQ_URL")
+
+	if staticRoot == "" {
+		staticRoot = defaultStaticRoot
+	}
+
+	var httpPort, err = strconv.Atoi(httpPortStr)
+	if err != nil {
+		httpPort = defaultHttpPort
+	}
+
+	if zeroMqUrl == "" {
+		zeroMqUrl = defaultZeroMqUrl
+	}
+
+	return DefconConfig{
+		StaticRoot: staticRoot,
+		HttpPort: httpPort,
+		ZeroMqUrl: zeroMqUrl,
+	}
+}
+
+var config = NewConfig()
 
 type appHandler func(http.ResponseWriter, *http.Request) error
 
@@ -28,7 +64,7 @@ func mainHandler(w http.ResponseWriter, r *http.Request) error {
 		path = "/index.html"
 	}
 
-	http.ServeFile(w, r, staticRoot+path)
+	http.ServeFile(w, r, config.StaticRoot+path)
 
 	return nil
 }
@@ -38,8 +74,6 @@ func onConnect(ns *socketio.NameSpace) {
 }
 
 func main() {
-	url := os.Getenv("DEFCON_ZEROMQ_URL")
-
 	sock_config := &socketio.Config{}
 	sock_config.HeartbeatTimeout = 2
 	sock_config.ClosingTimeout = 4
@@ -52,11 +86,9 @@ func main() {
 
 	sio.Handle("/", appHandler(mainHandler))
 
-	consumer := zero.NewZeroConsumer(url)
+	consumer := zero.NewZeroConsumer(config.ZeroMqUrl)
 	go consumer.Consume(sio.Broadcast)
 
-	port := os.Getenv("PORT")
-
-	fmt.Printf("[Web] Listening on 0.0.0.0:%v\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), sio))
+	fmt.Printf("[Web] Listening on 0.0.0.0:%v\n", config.HttpPort)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", config.HttpPort), sio))
 }
